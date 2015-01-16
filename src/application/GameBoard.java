1541cc7d75c57cd67f00f9dcf279e49342732de9
James Reinlein <jamesreinlein@gmail.com>
2015-01-15 21:07:24 -0500

    Game Logic
    Continued. :v:

--- src/application/GameBoard.java
+++ src/application/GameBoard.java
@@ -12,6 +12,15 @@ public class GameBoard {
 		board = new byte[numRows][numCols];
 	}
 
+	public int getNumRows() {
+		return numRows;
+	}
+
+
+	public int getNumCols() {
+		return numCols;
+	}
+
 	// constructor with custom size
 	public GameBoard(int numRows, int numCols) {
 		this.numRows = numRows;
@@ -34,8 +43,14 @@ public class GameBoard {
 	// 0 = empty, 1 = player #1, 2 = player #2
 	public boolean checkVictory(byte playerNumber) {
 		int rowToStop = numRows;
-		for (byte[] pos : getBoard()) {
-			
+		outerLoop:
+		for (int row = 0; row < numRows; row++) {
+			for (int col = 0; col < numCols; col++) {
+				if (board[row][col] == playerNumber) {
+					rowToStop = row;
+					break outerLoop;
+				}
+			}
 		}
 		
 		
