3b0a880a3243fd77195a63ba5fca48acca12a50d
James Reinlein <jamesreinlein@gmail.com>
2015-01-16 22:07:15 -0500

    Game Board continued
    Added function for playing a turn :v:

--- src/application/GameBoard.java
+++ src/application/GameBoard.java
@@ -4,6 +4,7 @@ public class GameBoard {
 	private int numRows, numCols; // default size
 	private int winCondition = 4;
 	private byte[][] board;
+	private byte playerTurn = 1;
 
 	// default constructor
 	public GameBoard() {
@@ -41,6 +42,29 @@ public class GameBoard {
 		return board;
 	}
 
+	/**
+	 * Finds the lowest possible empty position in the specified column and
+	 * places a chip of that player there
+	 * 
+	 * @param playerNumber
+	 *            Should be 1 or 2
+	 * @param col
+	 *            Column of board chip is dropped in
+	 */
+	public void play(byte playerNumber, int col) {
+		int lowestEmpty = numRows;
+		for (int i = numRows; i >= 0; i--) { // top to bottom
+			if (board[i][col] == 0)
+				lowestEmpty = i;
+		}
+		board[lowestEmpty][col] = playerNumber;
+		// change which player's turn it is (should only be 1 or 2)
+		if (playerTurn == 1)
+			playerTurn++;
+		else
+			playerTurn--;
+	}
+
 	// 0 = empty, 1 = player #1, 2 = player #2
 	public boolean checkVictory(byte playerNumber) {
 
@@ -52,9 +76,6 @@ public class GameBoard {
 		
 		if (checkDiagonalWin(playerNumber))
 			return true;
-		
-		
-		
 
 		return false;
 	}
